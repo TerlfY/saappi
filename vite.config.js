9efc9e5b3b2f13da12f5b09701e66034e4720e0f
TerlfY <141029876+TerlfY@users.noreply.github.com>
2025-04-22 14:58:10 +0300

    update vite.config to work with github pages and vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+// Determine base URL based on the environment
+// Use '/saappi/' if building in GitHub Actions (for GH Pages)
+// Use '/' for other environments (like Vercel, local dev)
+const viteBase = process.env.GITHUB_ACTIONS === "true" ? "/saappi/" : "/";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/saappi",
+  base: viteBase, // Use the dynamically determined base URL
 });
